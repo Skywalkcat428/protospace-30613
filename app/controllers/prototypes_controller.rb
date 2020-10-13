19801b8a4965b8e7172f7b9c471fce93061d00ea
Skywalkcat428 <k.inoue014@gmail.com>
2020-10-13 16:42:50 +0900

    ログアウトでerror発生

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,47 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-    @prototyoes = Prototyoe.all
+    @prototypes = User.all
   end
 
   def new
-    @prototyoes = Prototyoe.new
+    @prototypes = User.new
   end
 
+  def create
+    User.create(prototype_params)
+  end
+
+  def destroy
+    prototype = User.find(params[:id])
+    prototype.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    prototype = User.find(params[:id])
+    prototype.update(prototype_params)
+  end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile, :occupation, :position).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = User.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
