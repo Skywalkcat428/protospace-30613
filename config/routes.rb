ac4e9faf462f278ae326e1d8a0120ca5cf99f84a
Skywalkcat428 <k.inoue014@gmail.com>
2020-10-15 20:53:48 +0900

    詳細ページに投稿コメント表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :prototypes do
     resources :comments, only: :create
+  resources :users, only: :show
   end
-  
 end
