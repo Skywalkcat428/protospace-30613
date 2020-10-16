d647f2de7c1219484a92e0902c646baf2a38eade
Skywalkcat428 <k.inoue014@gmail.com>
2020-10-16 12:06:28 +0900

    ユーザー詳細ページ実装（修正）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @user = current_user
+    @user = User.find(params[:id])
     # @prototypes = current_user.prototypes
   end
 end
